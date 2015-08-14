b24d1f869cb4c3c21615b8bf2719defc8c82d0f4
Frédéric Persyn <frederic.persyn@gmail.com>
2015-08-14 07:44:14 +0200

    added new data title and price, used filter to format price data

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,9 @@
 app.controller('MainController',
 ['$scope', function($scope) {
 	$scope.title = 'Top Sellers in Books';
+	$scope.promo = 'Summer Sale';
+	$scope.product = {
+		name: 'The Book of Code',
+		price: 19,
+	};
 }]);
