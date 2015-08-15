92c8ae1b2a5f8145839b528d69d1cdb05f29cffe
Frédéric Persyn <frederic.persyn@gmail.com>
2015-08-15 12:04:13 +0200

    added likes property and plusone function to controller, added rating with ng-click directive to view
    * When <p class="likes"> is clicked, ng-click tells AngularJS to run the plusOne() function in the controller.
    * The plusOne() function gets the index of the product that was clicked, and then adds one to that product's likes property. plusOne() doesn't interact with the view at all; it just updates the controller. Any change made to the controller shows up in the view.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -7,13 +7,18 @@ app.controller('MainController',
 	    name: 'The Book of Trees', 
 	    price: 19, 
 	    pubdate: new Date('2014', '03', '08'), 
-	    cover: 'img/the-book-of-code.jpg' 
+	    cover: 'img/the-book-of-code.jpg',
+	    likes: 0,
 	  }, 
 	  { 
 	    name: 'Program or be Programmed', 
 	    price: 8, 
 	    pubdate: new Date('2013', '08', '01'), 
-	    cover: 'img/the-book-of-code.jpg' 
+	    cover: 'img/the-book-of-code.jpg',
+	    likes: 0,
 	  } 
 	];
+	$scope.plusOne = function(index) {
+		$scope.products[index].likes += 1;
+	};
 }]);
