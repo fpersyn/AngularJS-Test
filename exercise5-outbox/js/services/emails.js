a3b24828b19af5d8a30d67ea7a13ee8f3783514a
Frédéric Persyn <frederic.persyn@gmail.com>
2015-08-16 23:41:45 +0200

    created new outbox service, added it to the controller and displayed the data it in the view

---
+++ exercise5-outbox/js/services/emails.js
@@ -0,0 +1,9 @@
+app.factory('emails', ['$http', function($http) {
+	return $http.get('emails.json')
+		.success(function(data) {
+			return data;
+		})
+		.error(function(err) {
+			return err;
+		})
+}]);
