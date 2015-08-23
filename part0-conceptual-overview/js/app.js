b1b86b6a8a04f87f22bbef87ab6a6d01ef60322b
Frédéric Persyn <frederic.persyn@gmail.com>
2015-08-23 15:44:30 +0200

    refactored view-independent logic into finance service

--- part0-conceptual-overview/js/app.js
+++ part0-conceptual-overview/js/app.js
@@ -1,1 +1,1 @@
-var app = angular.module("invoice", []);
+var app = angular.module('invoice', ['finance']);
--- part0-conceptual-overview/js/controllers/InvoiceController.js
+++ part0-conceptual-overview/js/controllers/InvoiceController.js
@@ -1,21 +1,13 @@
-app.controller('InvoiceController', function() {
-	this.qty = 1;
-	this.cost = 1;
-	this.inCurr = 'EUR';
-	this.currencies = ['USD', 'EUR', 'CNY'];
-	this.usdToForeignRates = {
-		USD: 1,
-		EUR: 0.74,
-		CNY: 6.09,
-	};
+app.controller('InvoiceController', ['currencyConverter', function(currencyConverter) {
+  this.qty = 1;
+  this.cost = 1;
+  this.inCurr = 'EUR';
+  this.currencies = currencyConverter.currencies;
 
-	this.total = function total(outCurr) {
-		return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
-	};
-	this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
-		return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
-	};
-	this.pay = function pay() {
-		window.alert("Thanks!");
-	};
-});
+  this.total = function total(outCurr) {
+  	return currencyConverter.convert(this.qty * this.cost, this.inCurr, outCurr);
+  };
+  this.pay = function pay() {
+  	window.alert("Thanks!");
+  };
+}]);
